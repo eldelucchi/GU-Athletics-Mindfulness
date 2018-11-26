4e852b3e7158e6f5fcdc6bc382ea614dd91c6f97
Emma Delucchi <eldelucchi@gmail.com>
2018-11-26 13:50:42 -0800

    Initial Commit

---
+++ GU Athletics Mindfulness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GU Athletics Mindfulness
+//
+//  Created by Emma Delucchi on 11/26/18.
+//  Copyright © 2018 Emma Delucchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
