adf7c4d32af2dea44c7b5bba3221feb88f02d71a
Brewer Slack <bslack@zagmail.gonzaga.edu>
2018-12-08 16:46:50 -0800

    add sleepPicker. working on other pickers as well

--- GU Athletics Mindfulness/DayDetailTableViewController.swift
+++ GU Athletics Mindfulness/DayDetailTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DaysTableViewController.swift
+//  DayDetailTableViewController.swift
 //  GU Athletics Mindfulness
 //
 //  Created by Brewer Slack on 12/8/18.
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class DaysTableViewController: UITableViewController {
-
-    var days = [Day]()
+class DayDetailTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    let sleepHours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+    
+    @IBOutlet var sleepPicker: UIPickerView!
+    var day: Day? = nil
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        print("we're in")
+        self.sleepPicker.delegate = self
+        self.sleepPicker.dataSource = self
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -26,12 +32,23 @@ class DaysTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 5
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return days.count
+        return 1
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sleepHours.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sleepHours[row]
     }
 
     /*
